37548f94e86f5d7a394447eba37b54ffc84c7ba8
Arjun Bhamra <Arjun@Arjuns-Mac.local>
2017-12-05 15:28:14 +0530

    Added the quotes and wallpaper API's along with 'Next' button functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
 
+const API_KEY = '75a23ddcb004e1d05a70e46c083430569d302908392fb1007a7f5014307561f3';
+const WALLURL = "https://api.unsplash.com/photos/random?client_id="+API_KEY;
 window.onload = function(){//
-	console.log("Test");
+	fetchQuote();
+	fetchImage();
+
 }
 function myFunction(){
 	console.log("Button Click");
+	fetchQuote();
+	fetchImage();
+}
+
+function fetchQuote(){
+	console.log('fetch started');
+	let request = new XMLHttpRequest();
+	request.open("GET", "https://talaikis.com/api/quotes/random/", true);
+	request.onload=function(){
+		let response = request.responseText;
+		let result = JSON.parse(response);
+		console.log(result.quote);
+		console.log(result.author);
+		document.getElementById("quote").innerHTML = result.quote;
+		document.getElementById("author").innerHTML = "~"+result.author;	
+		
+	}
+
+	request.send();
+}
+
+
+
+
+function fetchImage(){
+	let wallRequest = new XMLHttpRequest();
+	console.log(WALLURL);
+	wallRequest.open("GET", WALLURL, true);
+	wallRequest.onload = function(){
+		let response = wallRequest.responseText;
+		let result = JSON.parse(response);
+		let imageUrl = result.urls['regular'];
+		console.log(result);
+		document.getElementById("quote-container").style.backgroundImage = 'url('+imageUrl+')';
+	}
+	wallRequest.send();
+	
+
 }
